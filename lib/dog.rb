9c02fc2fb54b0e5a7fb0aacfbec15ea8a313b1d3
Mars Le <hp.mars.le@gmail.com>
2019-11-11 00:51:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,7 +43,7 @@ class Dog
     if !dog.empty?
       dog_data = dog[0]
   #     dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
-      self.new_from_db(dog_data)
+      dog = self.new_from_db(dog_data)
     else
       dog = self.create(name: name, breed: breed)
     end
