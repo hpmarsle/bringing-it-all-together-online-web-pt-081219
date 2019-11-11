81a19fb0d26aecb2d825ba0d52515a40ecae2f04
Mars Le <hp.mars.le@gmail.com>
2019-11-11 00:43:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,7 +38,16 @@ class Dog
   #   self.find_by_name(name)
   #   self.create
   # end
-  
+  #   def self.find_or_create_by(name:, album:)
+  #   song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+  #   if !song.empty?
+  #     song_data = song[0]
+  #     song = Song.new(song_data[0], song_data[1], song_data[2])
+  #   else
+  #     song = self.create(name: name, album: album)
+  #   end
+  #   song
+  # end
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
     row = DB[:conn].execute(sql, name).flatten
