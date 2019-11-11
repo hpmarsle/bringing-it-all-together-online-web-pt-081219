80c53764e0ba258a33ca73a3f9c79937f4b8c42e
Mars Le <hp.mars.le@gmail.com>
2019-11-11 00:52:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,7 +42,6 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
-  #     dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
       dog = self.new_from_db(dog_data)
     else
       dog = self.create(name: name, breed: breed)
