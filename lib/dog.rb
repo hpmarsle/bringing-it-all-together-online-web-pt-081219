d8cb482a4493348abf4b3e198a772061c3b2366c
Mars Le <hp.mars.le@gmail.com>
2019-11-11 00:48:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,17 +38,18 @@ class Dog
   #   self.find_by_name(name)
   #   self.create
   # end
-  # def self.find_or_create_by(name:, breed:)
-  #   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-  #   if !dog.empty?
-  #     dog_data = dog[0]
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
   #     dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
-        self.new_from_db(dog_data)
-  #   else
-  #     dog = self.create(name: name, breed: breed)
-  #   end
-  #   dog
-  # end
+      self.new_from_db(dog_data)
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+  
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
     row = DB[:conn].execute(sql, name).flatten
